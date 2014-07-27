422f24a67f5aa4a72afec5075c11879c948f7502
McNeil <mcneil@gmx.net>
2014-07-27 23:08:30 +0200

    corrected tests

--- test/de/moralis/vs/CombatTest.java
+++ test/de/moralis/vs/CombatTest.java
@@ -40,8 +40,8 @@ public class CombatTest extends AbstractTest {
     }
 
     @Test
-    public void testWinnerRatioMiceVsMice() {
-        log.comment("Mice vs. Mice");
+    public void testWinnerRatioMice1VsMice2() {
+        log.comment("Mice1 vs. Mice2");
 
         Creature mice1 = new Mice();
         Creature mice2 = new Mice();
@@ -51,19 +51,7 @@ public class CombatTest extends AbstractTest {
 
         Combat combat = new Combat(mice1, mice2);
 
-        for (int i = 1; i <= 500; ++i) {
-            Creature winner = combat.fight();
-
-            if (Rat.class.isAssignableFrom(winner.getClass())) {
-                ++mice2Counter;
-            } else {
-                ++mice1Counter;
-            }
-        }
-
-        combat = new Combat(mice2, mice1);
-
-        for (int i = 1; i <= 500; ++i) {
+        for (int i = 1; i <= 1000; ++i) {
             Creature winner = combat.fight();
 
             if (winner.equals(mice2)) {
@@ -83,8 +71,39 @@ public class CombatTest extends AbstractTest {
     }
 
     @Test
-    public void testWinnerRatioRatVsRat() {
-        log.comment("Rat vs. Rat");
+    public void testWinnerRatioMice2VsMice1() {
+        log.comment("Mice1 vs. Mice2");
+
+        Creature mice1 = new Mice();
+        Creature mice2 = new Mice();
+
+        int mice1Counter = 0;
+        int mice2Counter = 0;
+
+        Combat combat = new Combat(mice2, mice1);
+
+        for (int i = 1; i <= 1000; ++i) {
+            Creature winner = combat.fight();
+
+            if (winner.equals(mice2)) {
+                ++mice2Counter;
+            } else {
+                ++mice1Counter;
+            }
+        }
+
+        double mice1Ratio = mice1Counter / 10d;
+        double mice2Ratio = mice2Counter / 10d;
+
+        log.info("mice1 wins: " + mice1Counter + " times");
+        log.info("mice2 wins: " + mice2Counter + " times");
+        log.info("mice1 ratio: " + mice1Ratio + "%");
+        log.info("mice2 ratio: " + mice2Ratio + "%");
+    }
+
+    @Test
+    public void testWinnerRatioRat1VsRat2() {
+        log.comment("Rat1 vs. Rat2");
 
         Creature rat1 = new Rat();
         Creature rat2 = new Rat();
@@ -94,7 +113,7 @@ public class CombatTest extends AbstractTest {
 
         Combat combat = new Combat(rat1, rat2);
 
-        for (int i = 1; i <= 500; ++i) {
+        for (int i = 1; i <= 1000; ++i) {
             Creature winner = combat.fight();
 
             if (winner.equals(rat2)) {
@@ -104,12 +123,31 @@ public class CombatTest extends AbstractTest {
             }
         }
 
-        combat = new Combat(rat2, rat1);
+        double rat1Ratio = rat1Counter / 10d;
+        double rat2Ratio = rat2Counter / 10d;
 
-        for (int i = 1; i <= 500; ++i) {
+        log.info("rat1 wins: " + rat1Counter + " times");
+        log.info("rat2 wins: " + rat2Counter + " times");
+        log.info("rat1 ratio: " + rat1Ratio + "%");
+        log.info("rat2 ratio: " + rat2Ratio + "%");
+    }
+
+    @Test
+    public void testWinnerRatioRat2VsRat1() {
+        log.comment("Rat1 vs. Rat2");
+
+        Creature rat1 = new Rat();
+        Creature rat2 = new Rat();
+
+        int rat1Counter = 0;
+        int rat2Counter = 0;
+
+        Combat combat = new Combat(rat2, rat1);
+
+        for (int i = 1; i <= 1000; ++i) {
             Creature winner = combat.fight();
 
-            if (Rat.class.isAssignableFrom(winner.getClass())) {
+            if (winner.equals(rat2)) {
                 ++rat2Counter;
             } else {
                 ++rat1Counter;
